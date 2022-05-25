3592832d51e0e144ab43b27bbd789cde06f38250
EnzoAbi1 <hiimensar@hotmail.com>
2022-05-25 14:13:51 +0300

    SemanticUi
    I used semantic ui buttons and an input field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import './App.css';
 import { useState } from 'react';
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'; 
 function App() {
-  const [text,setText] = useState();
+  const [text,setText]=useState(null);
+  const [druck,setDruck]=useState(false);
   let [fontsize, setFontSize] = useState(20);
-  function getText(evnt) {
-    setText(evnt.target.value)
+  function getText(val) {
+    setText(val.target.value)
+    setDruck(false);
   }
   return (
- <div>
-   <h1 className="head1">
-     Name:
-   </h1>
-   <h2>
-   <input className='input1' placeholder='MyName'  type='text' onChange={getText}/>
-</h2>
-<button className='btn1' onClick={() => setFontSize(fontsize + 5)}>+</button>
-<button className='btn2' onClick={() => setFontSize(fontsize - 5)}>-</button>
-<p className="TextHolder" style={{fontSize : fontsize}}> 
-{text}
-</p>
- </div>
+  <div>
+    <h1 className="head1">
+      Name:
+    </h1>
+    <div class="ui action input" className="input1" >
+      <input id='input2' placeholder="Enter the Text" type='text'  onChange={getText} />
+      <button class="ui button" onClick={() => setDruck(true)}>Get Text</button>
+    </div>
+    <div className='tryout'>
+      <button style={{fontSize : 15 }} class="ui button" id='+'  onClick={() => setFontSize(fontsize + 5)} >+</button>
+      <button style={{fontSize : 15 }}  class="ui button" id='-'  onClick={() => setFontSize(fontsize - 5)}>-</button>
+    </div>
+       { druck ?
+     <p className="TextHolder" style={{fontSize : fontsize}}> 
+       {text}
+     </p>  :null }
+
+  </div>
   );
 }
 
